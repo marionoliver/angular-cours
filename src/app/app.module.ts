8a8f228f60f599620a27c1a2e3e5f71e671e9e40
Marion OLIVER <marion.oliver@outlook.fr>
2018-12-17 13:08:26 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonComposantComponent } from './mon-composant/mon-composant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonComposantComponent
   ],
   imports: [
     BrowserModule
